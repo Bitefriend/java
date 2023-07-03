7c10bafbf9025dccf8f32f01b936154a226f1194
Bitefriend <119947716+Bitefriend@users.noreply.github.com>
2023-07-03 18:45:57 +0900

    Add files via upload

--- workspace_spring2/chapter04_MVC/src/test/main/java/org/joonzis/mapper/BoardMapper.java
+++ workspace_spring2/chapter04_MVC/src/test/main/java/org/joonzis/mapper/BoardMapper.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 import org.joonzis.domain.BoardVO;
+import org.joonzis.domain.Criteria;
 
 public interface BoardMapper {
-	public List<BoardVO> getList();
-	public int insert(BoardVO vo);
+	//public List<BoardVO> getList(); // 전체 데이터 조회
+	public int insert(BoardVO vo); 
 	public BoardVO read(long bno);
 	public int update(BoardVO vo);
 	public int delete(long bno);
-	
+	public List<BoardVO> getListPaging(Criteria cri); // 전체 데이터 조회(페이징)
+	public int getTotalCount();
 	
 }
